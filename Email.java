ead61492193c5ca73864e3168379f884564df49c
Zachary Cytryn <74995726+ZacharyCytryn@users.noreply.github.com>
2023-01-08 12:55:54 -0500

    Update Email.java

--- Email.java
+++ Email.java
@@ -1,11 +1,3 @@
-/**
- * Zachary Cytryn
- * ID: 114283379
- * Email: zachary.cytryn@stonybrook.edu
- * Homework #5
- * CSE 214 Recitation 30
- */
-
 package emailManager;
 import java.util.GregorianCalendar;
 
